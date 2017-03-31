98b4380382b71b6085e126b6c1752e05140dd2de
zerosum0x0 <zerosum0x0@gmail.com>
2017-03-31 14:50:01 -0600

    initial module skeleton

---
+++ modules/auxiliary/scanner/dcerpc/badlock_ms16_047.rb
@@ -0,0 +1,85 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+
+class MetasploitModule < Msf::Auxiliary
+
+  # Exploit mixins should be called first
+  include Msf::Exploit::Remote::DCERPC
+
+  include Msf::Auxiliary::Report
+
+  # Scanner mixin should be near last
+  include Msf::Auxiliary::Scanner
+
+  def initialize
+    super(
+      'Name'        => 'Badlock',
+      'Description' => %q{
+        This module can be used to obtain information from the Remote
+        Management Interface DCERPC service.
+      },
+      'Author'      => 'Sean Dillon <sean.dillon@risksense.com>',
+      'References' =>
+        [
+          [ 'CVE', '2016-0128'],
+          [ 'MSB', 'MS16-047'],
+          [ 'URL', 'https://technet.microsoft.com/en-us/library/security/ms16-047.aspx']
+        ],
+      'License'     => MSF_LICENSE
+    )
+
+    deregister_options('RHOST')
+
+    register_options(
+      [
+        Opt::RPORT(135)
+      ], self.class)
+  end
+
+  # Obtain information about a single host
+  def run_host(ip)
+    begin
+
+      op_sam2 = 0x39;
+      sam_uuid = '12345778-1234-abcd-ef00-0123456789ac'
+
+      connect
+      handle = dcerpc_handle(sam_uuid, '1.0', 'ncacn_np', [datastore['RPORT']])
+      dcerpc_bind(handle)
+
+      print_status("Bound to #{sam_uuid}")
+
+      count = (ip.length / 2).chr
+      data = ""
+      data << "\x00\x02\x00\x00"  # reference_id
+      data << count               # max count
+      data << "\x00"              # offset
+      data << count               # actual count
+      data << "\\" + ip           # name
+      data << "\x30"              # access mask
+
+      pkt = dcerpc.call(op_sam2, data)
+      status = ""
+
+      if status == "STATUS_SUCCESS" or status == "STATUS_ACCESS_DENIED":
+        print_warning("Host appears to be VULNERABLE to MS16-047 (Badlock)!")
+      else
+        print_status("Unable to determine if vulnerable, unexpected #{status}")
+      end
+
+    rescue ::Interrupt
+      raise $!
+    rescue ::Rex::Proto::SMB::Exceptions::LoginError
+      print_status("Host is NOT vulnerable (or anonymous login failed)")
+    rescue ::Rex::Proto::SMB::Exceptions::NoReply, Rex::Proto::DCERPC::Exceptions::NoResponse
+      print_status("The DCERPC service did not reply to our request")
+    rescue ::Exception => e
+      print_error("#{e.class}: #{e.message}")
+    end
+  end
+
+end
